604ada5f6b8eb05a4e53583df2d3501f7e0f6f13
lvas463 <lvas463@gmail.com>
2022-10-06 16:29:12 -0400

    Created Prob8.cpp for future reference.

---
+++ Prob8.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lvas4 on 10/6/2022.
+//
+
+// Create some sort of program...
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cout << "This is an incomplete program." << endl;
+
+    return 0;
+}
